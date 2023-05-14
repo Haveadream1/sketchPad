d9936015fadd32c1c520df60da72ae6feab14ac4
Haveadream1 <260602david@gmail.com>
2023-05-14 09:39:38 +0200

    Working Sketch

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
 let getValueRange = document.querySelector('.input-button').value;
 let gridChild = document.querySelectorAll('.grid-child');
-const gridContainer = document.querySelector('.grid-container');
-
+let gridContainer = document.querySelector('.grid-container');
 let getValueColor = document.querySelector('.input-color').value;
-console.log(getValueColor);
+let x;
+let alpha = 0;
+
+console.log('Select first the size of the grid!');
 
 const inputButton = document.querySelector('.input-button');
 function changeSize() {
+  restart();
   getValueRange = document.querySelector('.input-button').value;
-  console.log(getValueRange);
+  alpha = 0;
   createDiv();
   displayProportion();
+  clearFunction();
   return getValueRange;
 }
 inputButton.addEventListener('click', changeSize);
 
-function color(e) {
-  getValueColor = document.querySelector('.input-color').value;
-  e.target.style.backgroundColor = getValueColor;
-}
-// x change color | x clear | x color overflow
-// when eraser is active run function
-// same for rainbow
-// change the size call clear function 
 function createDiv() {
   numberGrid = getValueRange * getValueRange;
-  console.log(numberGrid);
+  console.log(`Number of grid : ${numberGrid}`);
   for(let i = 0; i < numberGrid; i++) {
     gridChild = document.createElement('div');
-
     let size = 400 / getValueRange; 
-    console.log(size);
+    console.log(`Size of the cell : ${size}`);
     gridChild.style.flexBasis = size + 'px';
     gridChild.style.height = size + 'px';
     gridChild.addEventListener('mouseover', color);
     gridContainer.appendChild(gridChild);
   };
 } 
-//
-const eraserButton = document.querySelector('.eraser-button');
-function eraserFunction(e) {
-  getValueColor = 'white';
-  e.target.style.backgroundColor = getValueColor;
-}
-//work only for one div
-function test() {
-  let x = 1
-  if(x === 1) {
-    numberGrid = getValueRange * getValueRange;
-    for(let i = 0; i < numberGrid; i++) {
-      gridChild.addEventListener('mouseover', eraserFunction);
-    };
-    console.log('hi');
+
+function choiceColor() {
+  if(x === 1 ) {
+    getValueColor = 'white';
+  } else if(x === 2) {
+    getValueColor = rainbowColor(array);
+  } else if(x === 3) {
+    getValueColor = document.querySelector('.input-color').value;
+  } else if(x === 4) {
+    if(alpha < 0.99) {
+      alpha += 0.1;
+    } else {
+      alpha = 0;
+      alpha += 0.1;
+    }
+    getValueColor = degradedFunction();
   }
 }
-eraserButton.addEventListener('click', test);
-//
 
-const textSize = document.querySelector('.text-size');
-function displayProportion() {
-  textSize.textContent = `${getValueRange} x ${getValueRange}`;
+function color(e) {
+    choiceColor();
+    e.target.style.backgroundColor = getValueColor;
 }
 
+const eraserButton = document.querySelector('.eraser-button');
+function eraserFunction() {
+  x = 1;
+}
+eraserButton.addEventListener('click', eraserFunction);
+
 function rainbowColor(array) {
+  x = 2;
   const randomIndex = Math.floor(Math.random() * array.length);
   const choice = array[randomIndex];
   return choice;
@@ -70,36 +71,52 @@ function rainbowColor(array) {
 const array = ['red','orange','yellow','green','blue','indigo','violet'];
 const rainbowButton = document.querySelector('.rainbow-button');
 rainbowButton.addEventListener('click', rainbowColor);
-rainbowColor(array);
 
-//doesnt work
+const colorInput = document.querySelector('.input-color');
+function colorPicker() {
+  x = 3;
+} 
+colorInput.addEventListener('click', colorPicker);
+
+const degradedButton = document.querySelector('.degraded-button');
+function degradedFunction() {
+  x = 4;
+  let red = 0;
+  let green = 0;
+  let blue = 0;
+  let rgbaValue = `(${red}, ${green}, ${blue}, ${alpha})`;
+  getValueColor = (`rgba${rgbaValue}`);
+  return getValueColor;
+}
+degradedButton.addEventListener('click', degradedFunction);
+
 let clearButton = document.querySelector('.clear-button');
 function clearFunction() {
-  for(key in gridChild) {
-    key.style.backgroundColor = 'white';
-  }
+  alpha = 0;
+  whiteDiv = document.querySelectorAll('div');
+  whiteDiv.forEach((item) => {
+    item.style.backgroundColor = 'white';
+  });
 }
 clearButton.addEventListener('click', clearFunction);
 
-
-
-// https://stackoverflow.com/questions/58955134/hover-over-one-element-at-the-time-with-dom-manipulation
-/*
-  gridChild.forEach((item) => {
-    item.style.backgroundColor = 'white';
+function restart() {
+  removeDiv = document.querySelectorAll('div');
+  removeDiv.forEach((item) => {
+    item.remove();
   });
-/*
-Set the value of input
-loop to create div
-color hover don't work
-*/
-/*
-Need a container with grid inside
-The number of grid should be determined by the range slide
-when we hover over the grid we should paint it
-the color should be determine with a button or a color circle
-Moreover we have an rainbow mode who choice a random color
-An eraser, so return white color
-and a clear button that target all the grid and make it blank
-don't forget to make something when the webpage reduce
-*/
+}
+
+const textSize = document.querySelector('.text-size');
+function displayProportion() {
+  textSize.textContent = `${getValueRange} x ${getValueRange}`;
+}
+
+/*  Need a container with grid inside
+    The number of grid should be determined by the range slide
+    when we hover over the grid we should paint it
+    the color should be determine with a button or a color circle
+    Moreover we have an rainbow mode who choice a random color
+    An eraser, so return white color
+    and a clear button that target all the grid and make it blank
+    don't forget to make something when the webpage reduce */
