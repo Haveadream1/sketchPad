0d7a2cb46962b5a23695b9e4107e1110c8dd4fff
Haveadream1 <260602david@gmail.com>
2025-02-28 16:04:01 +0100

    refactor: change to local variables and clean code

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
-let getValueRange = document.querySelector('.size-input').value;
-let getValueColor = document.querySelector('.color-input').value;
+let sizeInputValue = document.querySelector('.size-input').value;
+let colorInputValue = document.querySelector('.color-input').value;
 
 let gridChild = document.querySelectorAll('.grid-child');
 let gridSection = document.querySelector('.grid-section');
 
-let colorCode;
-let alpha = 0;
-
-
-const sizeButton = document.querySelector('.size-input');
-function changeSize() { // main function
-  restart();
-
-  getValueRange = document.querySelector('.size-input').value;
-  alpha = 0;
-
-  createCell();
-  displayProportion();
-  clearFunction();
-
-  return getValueRange;
-}
-sizeButton.addEventListener('click', changeSize);
-
-document.addEventListener("DOMContentLoaded", () => { // Run on the onload with base value 32
-  changeSize()
-})
 
 function createCell() {
-  numberCell = getValueRange * getValueRange;
+  numberCell = sizeInputValue * sizeInputValue;
   console.log(`Number of cell : ${numberCell}`);
 
   for(let i = 0; i < numberCell; i++) {
     cell = document.createElement('div');
     gridSize = 400;
 
-    let size = gridSize / getValueRange; 
+    let size = gridSize / sizeInputValue; 
     console.log(`Size of the cell : ${size}`);
 
     cell.style.flexBasis = size + 'px'; // Width of the cell
@@ -48,11 +26,11 @@ function createCell() {
 
 function choiceColor() { // Color choice depends on the button
   if(colorCode === 1 ) {
-    getValueColor = 'white'; // Eraser color
+    colorInputValue = 'white'; // Eraser color
   } else if(colorCode === 2) {
-    getValueColor = rainbowFunction(array);
+    colorInputValue = rainbowFunction(array);
   } else if(colorCode === 3) {
-    getValueColor = document.querySelector('.color-input').value;
+    colorInputValue = document.querySelector('.color-input').value;
   } else if(colorCode === 4) { // Fade color from transparent to solid
     if(alpha < 0.99) {
       alpha += 0.1;
@@ -60,13 +38,13 @@ function choiceColor() { // Color choice depends on the button
       alpha = 0;
       alpha += 0.1;
     }
-    getValueColor = fadeFunction();
+    colorInputValue = fadeFunction();
   }
 }
 
 function color(e) {
     choiceColor();
-    e.target.style.backgroundColor = getValueColor;
+    e.target.style.backgroundColor = colorInputValue;
 }
 
 const eraserButton = document.querySelector('.eraser-button');
@@ -89,42 +67,55 @@ rainbowButton.addEventListener('click', rainbowFunction);
 
 const colorInput = document.querySelector('.color-input');
 function colorPicker() {
-  colorCode = 3;
+  let colorCode = 3;
 } 
 colorInput.addEventListener('click', colorPicker);
 
 const fadeButton = document.querySelector('.fade-button');
-function fadeFunction() { // Create a rbga value with only the transparency modifiable
-  colorCode = 4;
+function fadeColor() { // Create a rbga value with only the transparency modifiable
+  let colorCode = 4;
 
   let red, green, blue;
   let rgbaValue = `${red = 0}, ${green = 0}, ${blue = 0}, ${alpha}`;
-  getValueColor = `rgba${rgbaValue}`; // Convert to a string
+  let colorInputValue = `rgba${rgbaValue}`; // Convert to a string
 
-  return getValueColor;
+  return colorInputValue;
 }
-fadeButton.addEventListener('click', fadeFunction);
+fadeButton.addEventListener('click', fadeColor);
 
 const clearButton = document.querySelector('.clear-button');
-function clearFunction() { // Select all the cell to clear the color
-  gridCell = document.querySelectorAll('div');
-  gridCell.forEach((cell) => {
-    cell.style.backgroundColor = 'white';
-  });
+function clearColor() { // Clear the grid of all colors
+  let gridCells = document.querySelectorAll('div');
+  gridCells.forEach((cell) => cell.style.backgroundColor = 'white');
 }
-clearButton.addEventListener('click', clearFunction);
+clearButton.addEventListener('click', clearColor);
 
-function restart() { // Clear the grid of all cells
-  removeCell = document.querySelectorAll('div');
-  removeCell.forEach((cell) => {
-    cell.remove();
-  });
+function removeCell() { // Remove the grid of all cells
+  let gridCells = document.querySelectorAll('div');
+  gridCells.forEach((cell) => cell.remove());
 }
 
 const sizeText = document.querySelector('.size-text');
-function displayProportion() {
-  sizeText.textContent = `${getValueRange} x ${getValueRange}`;
+const displayProportion = () => sizeText.textContent = `${sizeInputValue} x ${sizeInputValue}`;
+
+const sizeInput = document.querySelector('.size-input');
+function changeGridSize() { // main function
+  removeCell();
+
+  let sizeInputValue = document.querySelector('.size-input').value;
+  let alpha = 0;
+
+  createCell();
+  displayProportion();
+  clearColor();
+
+  return sizeInputValue, alpha;
 }
+sizeInput.addEventListener('click', changeGridSize);
+
+document.addEventListener("DOMContentLoaded", () => { // Run on the onload with base value 32
+  changeGridSize();
+})
 
 /*  Need a container with grid inside
     The number of grid should be determined by the range slide
